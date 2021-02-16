98eae27f3c07c2b05c16432c3c67c73822dd9453
RaquelRB <raquelrobles95@gmail.com>
2021-02-16 17:24:18 +0100

    first push

---
+++ app.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const path         = require('path');
+const cors = require("cors")
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'))
+app.use(express.json())
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+// Middleware Setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+
+//CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+app.use(cors({
+  credentials: true,
+  origin: ["http://localhost:3000"]
+}));
+      
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+app.use('/sendtome', require('./routes/sendToMe'))
+
+
+module.exports = app;
---
+++ routes/sendToMe.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const sendToMeRouter  = express.Router();
+const nodemailer = require('nodemailer')
+
+const transport = {
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAILPASSWORD
+  }
+};
+
+const transporter = nodemailer.createTransport(transport);
+  transporter.verify((error, success) => {
+    if(error) {
+      console.error(error)
+    } else {
+      console.log('nodemailer transport success')
+    }
+  });
+
+  sendToMeRouter.post('/', (req,res, next) => {
+
+    const mail = {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: req.body.email.subject,
+      text: `
+      from:
+      ${req.body.email.name} 
+
+      contact: ${req.body.email.email}
+
+      message: 
+
+      ${req.body.email.text}`
+    }
+ 
+    transporter.sendMail(mail, (err,data) => {
+      if(err) {
+        res.json({
+          status: 'Ha habido un error al enviar la consulta. Por favor, inténtalo de nuevo más tarde.'
+        })
+      } else {
+        res.json({
+          status: 'Tu consulta se ha enviado correctamente.'
+        })
+      }
+    })
+
+  });
+
+
+module.exports = sendToMeRouter;
